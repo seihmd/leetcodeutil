305277cc57ff02713687e38cd1d3a56c8506224b
seihmd <moku.said@gmail.com>
2018-04-21 21:22:33 +0900

    linked list

---
+++ linkedlist.go
@@ -0,0 +1,51 @@
+package leetcodeutil
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// ListNode struct
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// LinkedList generates *ListNode
+func LinkedList(vals []int) *ListNode {
+	if len(vals) == 0 {
+		return nil
+	}
+	root := &ListNode{
+		Val: vals[0],
+	}
+	current := root
+	for i := 1; i < len(vals); i++ {
+		next := &ListNode{
+			Val: vals[i],
+		}
+		current.Next = next
+		current = next
+	}
+	return root
+}
+
+func (ln *ListNode) String() (r string) {
+	c := ln
+	for c != nil {
+		r += strconv.Itoa(c.Val) + " "
+		c = c.Next
+	}
+	r = strings.TrimSuffix(r, " ")
+	return
+}
+
+// Print outputs *ListNode.String()
+func (ln *ListNode) Print() {
+	if ln == nil {
+		fmt.Println(nil)
+		return
+	}
+	fmt.Println(ln.String())
+}
---
+++ test/linkedlist_test.go
@@ -0,0 +1,48 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/seihmd/leetcodeutil"
+)
+
+func TestEmptyLinkedList(t *testing.T) {
+	l := leetcodeutil.LinkedList(nil)
+	if l != nil {
+		t.Error("LinkedList should be nil")
+	}
+}
+
+func TestLinkedList(t *testing.T) {
+	l := leetcodeutil.LinkedList([]int{1, 2, 3})
+
+	type testdata struct {
+		listnode *leetcodeutil.ListNode
+		expect   int
+	}
+	tests := []testdata{
+		{l, 1},
+		{l.Next, 2},
+		{l.Next.Next, 3},
+	}
+
+	for i, test := range tests {
+		if test.listnode.Val != test.expect {
+			t.Errorf("ListNode: %d, Val expects %d, actual %d ", i, test.expect, test.listnode.Val)
+		}
+	}
+}
+
+func TestEmptyLinkedListString(t *testing.T) {
+	l := leetcodeutil.LinkedList(nil)
+	if l.String() != "" {
+		t.Error("String should be empty")
+	}
+}
+
+func TestListNodeString(t *testing.T) {
+	l := leetcodeutil.LinkedList([]int{1, 2, 3})
+	if l.String() != "1 2 3" {
+		t.Errorf("LinkedList.String expects '1 2 3', actual '%s'", l.String())
+	}
+}
